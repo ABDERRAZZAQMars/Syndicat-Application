775d4f2b88af827eb0f853dfb99e048760bdbd04
abderrazzaq mars <abderrazzaqmars99@gmail.com>
2023-01-04 00:45:57 +0100

    Create Function de Register

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -9,9 +9,46 @@ const cookie = require('cookie-parser')
 // method : post
 // url : api/auth/register
 // acces : Public
-const Register = (req, res) => {
-    return res.status(200).json({ message: 'this function Register' })
-}
+const Register = asyncHandler(async (req, res) => {
+    const { name, email, password, token, verified, role } = req.body
+    if (!name || !email || !password) {
+        return res.status(200).json({ message: 'Please ADD All Fields' })
+    }
+    // Check if user exists
+    userExists = await User.findOne({ email })
+    const roles = await RoleModel.findOne({ role })
+    if (userExists) {
+        return res.status(200).json({ message: 'User already exists' })
+    }
+    // Hashed Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+        token: generateToken(),
+        verified,
+        role: roles.id
+    })
+
+    if (user) {
+        if (user.verified == false) {
+            sendConfirmationEmail(
+                user.name,
+                user.email,
+                user.token,
+                user.id,
+                user.verified
+            );
+            return res.status(200).send({ message: "Pending Account. Please Verify Your Email!" })
+        }
+    } else {
+        return res.status(400).json({ message: 'Invalid user data' })
+    }
+})
 
 // method : post
 // url : api/auth/login
