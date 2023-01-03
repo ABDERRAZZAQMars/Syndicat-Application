5e6b011964eb6c6536092f0d3b048d2213855194
abderrazzaq mars <abderrazzaqmars99@gmail.com>
2023-01-04 00:31:22 +0100

    Verify Token

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -58,6 +58,18 @@ const generateToken = (id) => {
     })
 }
 
+// Verify Token
+const Verify = async (req, res) => {
+    const token = req.params.token
+    const id = req.params.id
+    const user = await User.findById(id)
+    if (user.verified == false && user.token == token) {
+        user.verified = true
+        user.save()
+        return res.status(200).json({ message: 'Token is verified' })
+    } else { return res.status(400).send('token not valid') }
+}
+
 module.exports = {
     Login,
     Register,
