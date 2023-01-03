dc4ee13f4aea283a72f0c4907fa1a8ebbded0dfd
abderrazzaq mars <abderrazzaqmars99@gmail.com>
2023-01-04 00:36:02 +0100

    create function forget password

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -40,9 +40,22 @@ const Login = asyncHandler(async (req, res) => {
 // method : post
 // url : api/auth/forgetpassword
 // acces : Public
-const ForgetPassword = (req, res) => {
-    return res.status(200).json({ message: 'this function Forget Password' })
-}
+const ForgetPassword = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    if (!email) {
+        return res.status(400).json({ message: 'Please ADD field' })
+    }
+    const user = await User.findOne({ email })
+    if (user) {
+        let token = generateToken(user._id)
+        token = token.split(".").join("")
+        user.token = token
+        user.save()
+        await resetPasswordEmail(user.name, user.email, user.token)
+        return res.status(200).send('plaise check your email for reset your password of email')
+    }
+    return res.status(400).json({ message: 'Invalid email' })
+})
 
 // method : post
 // url : api/auth/resetpassword/:token
