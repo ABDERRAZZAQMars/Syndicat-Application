c1178865ff713c31b7b59f71e3e16a6f2d157129
abderrazzaq mars <abderrazzaqmars99@gmail.com>
2023-01-04 10:52:14 +0100

    add Route de verification Token

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -2,14 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 //Les Function Authentification
-const { Login, Register, ForgetPassword, ResetPassword, Verify } = require('../controllers/AuthController')
+const {login,register,forgetPassword,resetPassword,verifyToken} = require('../controllers/Authentification/indexAuth')
 
 //url : /api/auth
-router.post('/login', Login)
-router.post('/register/', Register)
-router.post('/forgetpassword', ForgetPassword)
-router.post('/resetpassword/:token', ResetPassword)
-router.get('/user/:id/confirm/:token', Verify)
+router.post('/login', login)
+router.post('/register/', register)
+router.post('/forgetpassword', forgetPassword)
+router.post('/resetpassword/:token', resetPassword)
+router.get('/user/:id/confirm/:token', verifyToken)
 
 
 module.exports = router
