6776cb12d10a757f1f95b8a4932d44c3d8d8285b
abderrazzaq mars <abderrazzaqmars99@gmail.com>
2023-01-06 09:28:05 +0100

    add icons

---
+++ server/models/syndicat/ApartmentModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const apartmentSchema = new mongoose.Schema({
+  number: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  building: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Building',
+  },
+  tenant: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Client',
+  },
+  status: {
+    type: String,
+    enum: ['vacant', 'occupied'],
+    default: 'vacant',
+  },
+});
+
+module.exports = mongoose.model('Apartment', apartmentSchema);
