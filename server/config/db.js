77593283823cdfba9dd30ec5d9b26663aa5a0457
abderrazzaq mars <abderrazzaqmars99@gmail.com>
2023-01-15 19:52:32 +0100

    fixé error cnx

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected to DATABASE ");
 }).catch(() => {
